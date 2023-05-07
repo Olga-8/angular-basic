32061066d908b9faff3709beb2293fb706de6965
Olga Morozova <olhamorozowa@gmail.com>
2023-05-07 18:40:06 +0200

    use ngStyle

--- my-app/src/app/products/products.component.ts
+++ my-app/src/app/products/products.component.ts
@@ -10,6 +10,7 @@ export class ProductsComponent {
     {
       id: 1,
       name: 'Apple',
+      avaliable: 'Avaliable',
       price: 100,
       description: 'This is an apple',
       image: 'https://freesvg.org/img/Stylized-Apple-Leaves.png'
@@ -17,6 +18,7 @@ export class ProductsComponent {
     {
       id: 2,
       name: 'Banana',
+      avaliable: 'Avaliable',
       price: 200,
       description: 'This is a banana',
       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Bananas.svg/1280px-Bananas.svg.png'
@@ -24,6 +26,7 @@ export class ProductsComponent {
     {
       id: 3,
       name: 'Mango',
+      avaliable: 'Avaliable',
       price: 400,
       description: 'This is a mango',
       image: 'https://www.svgrepo.com/show/397130/mango.svg'
@@ -31,6 +34,7 @@ export class ProductsComponent {
     {
       id: 4,
       name: 'Pineapple',
+      avaliable: 'Not avaliable',
       price: 500,
       description: 'This is a pineapple',
       image: 'https://www.svgheart.com/wp-content/uploads/2021/11/pineapple-summer-fruit-free-svg-file-SvgHeart.Com-2.png'
